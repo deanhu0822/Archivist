d24d54fd7f6af487e6728a10e35c69e562e5455f
shashankgp15 <112502999+shashankgp15@users.noreply.github.com>
2023-02-04 18:35:11 -0500

    Delete joshua_s_application1-v1.0.1 directory

--- joshua_s_application1-v1.0.1/lib/presentation/archives_screen/binding/archives_binding.dart
+++
@@ -1,9 +0,0 @@
-import '../controller/archives_controller.dart';
-import 'package:get/get.dart';
-
-class ArchivesBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => ArchivesController());
-  }
-}
--- joshua_s_application1-v1.0.1/lib/presentation/selection_screen/binding/selection_binding.dart
+++
@@ -1,9 +0,0 @@
-import '../controller/selection_controller.dart';
-import 'package:get/get.dart';
-
-class SelectionBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => SelectionController());
-  }
-}
